4e1ce3a21611a876fe2216fb8338bc30d9ab3e08
Jamie Toh <s10194918@connect.np.edu.sg>
2020-05-08 22:06:07 +0800

    Update

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,25 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer media = MediaPlayer.create(this, R.raw.baby_laughing);
+        media.start();
+
+
+        VideoView view = findViewById(R.id.videoView);
+        view.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        view.start();
     }
 }
